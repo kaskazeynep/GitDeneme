11305de7d97fbbb3d361735a16e489e7b6e7f490
zeynep.kaska <zeynep.kaska@ANK1-YZLMORT-07>
2025-01-27 11:36:20 +0300

    deneme devam

--- GitDeneme/Program.cs
+++ GitDeneme/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Githuba Hoşgeldinizz !");
+Console.WriteLine("Githuba Hoşgeldinizz !");
+Console.WriteLine("Githuba Hoşgeldinizz !");
+Console.WriteLine("Githuba Hoşgeldinizz !");
